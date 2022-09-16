1f591baf5b7c0e585a4eb1f8f8da1c835a3db3d8
asset153 <matadamczyk401@gmail.com>
2022-09-16 18:59:35 +0200

    user login logic;

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,28 +1,56 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { supabase } from "../../supabaseClient";
 import { useNavigate } from "react-router-dom";
 
 function Header() {
+  const user = supabase.auth.user();
+
+  console.log(user);
+
   const navigation = useNavigate();
+
+  const signOut = async () => await supabase.auth.signOut();
+
+  const handleClickSignOut = async function () {
+    navigation("/SignIn");
+    signOut();
+  };
+
+  const userIsLogin = (
+    <nav className="btn-group">
+      <button
+        onClick={handleClickSignOut}
+        type="button"
+        className="btn btn-outline-light"
+      >
+        Sign Out
+      </button>
+    </nav>
+  );
+
+  const userIsNotLogin = (
+    <nav className="btn-group">
+      <button
+        onClick={() => navigation("/SignUp")}
+        type="button"
+        className="btn btn-outline-light"
+      >
+        Sign Up
+      </button>
+      <button
+        onClick={() => navigation("/SignIn")}
+        type="button"
+        className="btn btn-outline-light"
+      >
+        Sign In
+      </button>
+    </nav>
+  );
 
   return (
     <header className="messChat__header container-fluid bg-primary text-light d-flex justify-content-between align-items-center">
       <h1>MessChat</h1>
-      <nav className="btn-group">
-        <button
-          onClick={() => navigation("/SignUp")}
-          type="button"
-          className="btn btn-outline-light"
-        >
-          Sign Up
-        </button>
-        <button
-          onClick={() => navigation("/SignIn")}
-          type="button"
-          className="btn btn-outline-light"
-        >
-          Sign In
-        </button>
-      </nav>
+      {user ? userIsLogin : userIsNotLogin}
     </header>
   );
 }
--- src/SignIn/SignIn.jsx
+++ src/SignIn/SignIn.jsx
@@ -1,7 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
+import { supabase } from "../../supabaseClient";
+import { useNavigate } from "react-router-dom";
+import Header from "../Header/Header";
 
 function SignIn() {
-  return <div>SignIn</div>;
+  const navigate = useNavigate();
+  const [signInValue, setSignInValue] = useState({
+    email: "mat@mat.pl",
+    password: "123456",
+  });
+
+  const { email, password } = signInValue;
+
+  const handleChangeSignInValue = function (e) {
+    const { name, value } = e.target;
+    setSignInValue((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  };
+
+  const userSignIn = async function () {
+    try {
+      const { user, session, error } = await supabase.auth.signIn({
+        email,
+        password,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleClickSignIn = async function () {
+    // navigate("/");
+    userSignIn();
+  };
+
+  return (
+    <>
+      <Header />
+      <div className="container vh-100">
+        <div className="row h-75">
+          <form className="border col-md-6 p-5 m-auto form-group">
+            <fieldset className="">
+              <legend className="fs-3 fw-bold text-primary">Sign In:</legend>
+
+              <div className="d-flex flex-column">
+                <label className="form-label" htmlFor="">
+                  Email address:
+                </label>
+                <input
+                  name="email"
+                  value={email}
+                  onChange={handleChangeSignInValue}
+                  className="form-control"
+                  type="text"
+                />
+              </div>
+
+              <div className="d-flex flex-column my-2">
+                <label className="form-label" htmlFor="">
+                  Password:
+                </label>
+                <input
+                  name="password"
+                  value={password}
+                  onChange={handleChangeSignInValue}
+                  className="form-control"
+                  type="text"
+                />
+              </div>
+
+              <button
+                onClick={handleClickSignIn}
+                type="button"
+                className="btn btn-primary mt-3"
+              >
+                Sign In
+              </button>
+            </fieldset>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default SignIn;
